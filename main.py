43762cb976330f250759b4f429ea32bd08422364
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-10-07 08:18:35 -0400

    woups

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from graph import Graph
 from ops import generate_subgraph
 
 from node_editor import graph as shared_graph
+from solvers.example import ExampleSolver
 
 
 def main():
     subgraph1: Graph = generate_subgraph(shared_graph)
     subgraph2: Graph = generate_subgraph(shared_graph)
 
-    print(subgraph1 == subgraph2)
+    print(shared_graph == ExampleSolver().solve(subgraph1, subgraph2))
 
 
 if __name__ == '__main__':
